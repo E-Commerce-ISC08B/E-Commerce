33733a48d4267a4ee22ee7f6190b6c08c82bdae4
Carolina <up210042@alumnos.upa.edu.mx>
2024-07-16 19:29:13 -0600

    navbar (primera implementacion)

---
+++ frontend/src/components/Account/Account.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Account = () => {
+  return (
+    <div>
+      <h2>Account Details</h2>
+      {/* Mostrar detalles de la cuenta del usuario */}
+    </div>
+  );
+};
+
+export default Account;
