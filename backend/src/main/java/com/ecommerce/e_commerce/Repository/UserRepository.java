898a176cd26e6e4f9f2051f89e79a74e87623c91
José Alberto Flores Martínez <up210079@alumnos.upa.edu.mx>
2024-07-31 12:10:39 -0600

    Query

--- backend/src/main/java/com/ecommerce/e_commerce/Repository/UserRepository.java
+++ backend/src/main/java/com/ecommerce/e_commerce/Repository/UserRepository.java
@@ -3,11 +3,13 @@ package com.ecommerce.e_commerce.Repository;
 import com.ecommerce.e_commerce.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query; 
+import org.springframework.data.repository.query.Param; 
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     @Query("SELECT u FROM Users u WHERE u.customerName = :customerName")
     List<Users> findByCustomerName(@Param("customerName") String customerName);
-
 }
