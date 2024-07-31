fa676444c07ffc46cb304518996254a4b2d66016
UP210713 <up210713@alumnos.upa.edu.mx>
2024-07-31 15:00:40 -0600

    Errores y CRUD

--- backend/src/main/java/com/ecommerce/e_commerce/controller/ProductController.java
+++ backend/src/main/java/com/ecommerce/e_commerce/controller/ProductController.java
@@ -9,13 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.http.HttpStatus;
 import com.ecommerce.e_commerce.model.Product;
 import com.ecommerce.e_commerce.service.ProductService;
 
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @Controller
@@ -48,7 +49,8 @@ public class ProductController {
         return ResponseEntity.ok(productService.getProducts());
     }
 
-    @GetMapping("/eliminar/{id}")  // Eliminar Boton
+    @GetMapping("/eliminar/{id}") 
+    @ResponseStatus(HttpStatus.OK) // Eliminar Boton
     public String deleteProduct(@PathVariable Long id ) {
         if (id > 0 ) {
             productService.deleteProduct(id);
@@ -57,8 +59,14 @@ public class ProductController {
     } 
 
     @GetMapping("/update/{id}")
-    public ResponseEntity<Optional<Product>> updateProduct(@PathVariable Long id){
-        return ResponseEntity.ok(productService.getProduct(id));
+    public String updateProduct(@PathVariable Long id, Model model) {
+        Optional<Product> product = productService.getProduct(id);
+        if (product.isPresent()) {
+            model.addAttribute("product", product.get());
+            return "crud"; // Retorna la vista "crud" con el producto
+        } else {
+            return "redirect:/"; // Redirige a la vista de productos si el producto no existe
+        }
     }
     @GetMapping("/name/{productName}")
     public List<Product> getProductsByName(@PathVariable String productName) {
