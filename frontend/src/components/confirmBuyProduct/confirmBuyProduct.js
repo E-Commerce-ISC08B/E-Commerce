e96f24d985b8c4f3e8054d508599fa8b0293e9c5
José Alberto Flores Martínez <up210079@alumnos.upa.edu.mx>
2024-08-01 21:49:03 -0600

    WebConfig

--- frontend/src/components/confirmBuyProduct/confirmBuyProduct.js
+++ frontend/src/components/confirmBuyProduct/confirmBuyProduct.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './confirmBuyProduct.css';
+import { useState } from 'react';
 
-const ConfirmBuyProduct = ({ productDetails }) => {
+const ConfirmBuyProduct = (props) => {
+
+const [productDetails , setProductDetails] = useState([]); 
+
+// Realizar una solicitud GET
+
+useEffect(()=>{
+  fetch('http://localhost:8080/allproducts')
+  .then(response => response.json())
+  .then(data => setProductDetails(data))
+  .catch(error => console.error('Error:', error));
+})
+
   // Verifica que productDetails esté definido y tenga propiedades
-  if (!productDetails) {
-    return <div>No se encontraron detalles del producto.</div>;
-  }
+  //if (!productDetails) {
+    //return <div>No se encontraron detalles del producto.</div>;
+ // }
 
-  const { productID, productName, productPrice, productQTY, description } = productDetails;
+
 
   return (
     <div className="confirm-container">
       <h2>Confirmar Compra</h2>
+      {
+        productDetails.map((detail)=>{
       <div className="product-details">
-        <p><strong>ID del Producto:</strong> {productID}</p>
-        <p><strong>Nombre del Producto:</strong> {productName}</p>
-        <p><strong>Precio:</strong> ${productPrice}</p>
-        <p><strong>Cantidad:</strong> {productQTY}</p>
-        <p><strong>Descripción:</strong> {description}</p>
+        
+       
+        <p><strong>ID del Producto:</strong> {detail.productID}</p>
+        
+        <p><strong>Nombre del Producto:</strong> {detail.productName}</p>
+        <p><strong>Precio:</strong> ${detail.productPrice}</p>
+        <p><strong>Cantidad:</strong> {detail.productQTY}</p>
       </div>
+       })}
       <button className="confirm-button">Confirmar</button>
     </div>
   );
 };
+
 
 export default ConfirmBuyProduct;
